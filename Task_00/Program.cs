ed88bc2a52578ac28e93d81693d7f5531a9fe1e6
Lilith <ll.file.top.secret@yandex.ru>
2022-11-26 16:48:56 +0300

    add Task_00 - demonstrate

---
+++ Task_00/Program.cs
@@ -0,0 +1,7 @@
+﻿// 0. Напишите программу, которая на входе принимает число 
+// и выдает его квадрат
+
+int number = int.Parse(Console.ReadLine());
+
+int quarter = number * number;  
+Console.WriteLine(quarter);
