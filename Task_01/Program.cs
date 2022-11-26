9c8110f03d0761096ef17a9c1ceb9a6f06717c21
Lilith <ll.file.top.secret@yandex.ru>
2022-11-26 17:09:59 +0300

    add Task_01

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿// Напишите программу, которая на вход принимает 2 числа 
+// и проверяет, является ли первое число квадратом второго.
+//a = 25, b = 5 -> да a = 2, b = 10 -> нет a = 9, b = -3 -> да a = -3 b = 9 -> нет
+
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+
+if(numberA == numberB * numberB)
+{
+    Console.WriteLine("верно");
+}
+else
+{
+    Console.WriteLine("неверно");
+}
