09f1cee172ae7f785a1425bb50a5c7dcfd28b5d9
Lilith <ll.file.top.secret@yandex.ru>
2022-11-28 21:33:07 +0300

    add Task_12

---
+++ Task_12/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход 
+// координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09 A (7,-5); B (1,-1) -> 7,21
+
+bool isParsedXA = int.TryParse(Console.ReadLine(), out int xa);
+
+/* if (!isParsedXA)
+{
+    Console.WriteLine("Координаты ввели не корректно");
+    return;
+} */
+bool isParsedYA = int.TryParse(Console.ReadLine(), out int ya);
+if (!isParsedYA)
+{
+    Console.WriteLine("Координаты ввели не корректно");
+    return;
+}
+bool isParsedXB = int.TryParse(Console.ReadLine(), out int xb);
+if (!isParsedXB)
+{
+    Console.WriteLine("Координаты ввели не корректно");
+    return;
+}
+bool isParsedYB = int.TryParse(Console.ReadLine(), out int yb);
+if (!isParsedYB)
+{
+    Console.WriteLine("Координаты ввели не корректно");
+    return;
+}
+
+double GetDistanceBetweenToPoints(double xa, double ya, double xb, double yb)
+
+{
+    double ab = Math.Sqrt((xb - xa) * (xb - xa) - (yb - ya) * (yb - ya));
+    return ab;
+}
+double res = GetDistanceBetweenToPoints(xa, ya, xb, yb);
+
+Console.WriteLine(res);
