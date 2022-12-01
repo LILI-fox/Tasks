90b004f52d057dace4825e2aa394fa6123b2a6f1
Lilith <ll.file.top.secret@yandex.ru>
2022-12-01 21:27:49 +0300

    add Task_15

---
+++ Task_15/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 26: Напишите программу, которая принимает на вход число 
+// и выдаёт количество цифр в числе.
+// 456 -> 3 78 -> 2 89126 -> 5
+
+ int number = int.Parse(Console.ReadLine());
+
+int result = GetNumberOfDigits(number);
+
+Console.WriteLine(result);
+
+int GetNumberOfDigits(int number)
+{
+    int i =0;
+    for(; number>0;i++)
+    {
+        number/=10;
+    }
+    return i;
+} 
+
+// Задача 30: Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
