12c7cfa7b90b2e2ef0f53ef309b16a0e4812b041
Lilith <ll.file.top.secret@yandex.ru>
2023-01-09 02:42:03 +0300

    add Task_27 + delete old names files

---
+++ Task_27_CopyArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using static Common.Helper;
+
+// Задача 45: Напишите программу, которая будет создавать 
+// копию заданного массива с помощью поэлементного копирования.
+
+int[] initialArray = GenerateArray(-10, 10, 5);
+
+PrintArray(initialArray);
+
+int[] copiedArray = CopyArray(initialArray);
+
+PrintArray(copiedArray);
+
+int[] CopyArray(int[] initialArray)
+{
+    int[] newArr = new int[initialArray.Length];
+    for (int i = 0; i < initialArray.Length; i++)
+    {
+        newArr[i] = initialArray[i];
+    }
+    return newArr;
+}
